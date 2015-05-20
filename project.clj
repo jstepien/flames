e73cb668812b6fb718d45f78b490c5e95c275309
Jan Stępień <jstepien@users.sourceforge.net>
2015-05-20 22:21:33 +0200

    Trim the dependency tree

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject flames "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.2"]
+                 [ring/ring-core "1.3.2"
+                  :exclusions [clj-time
+                               commons-fileupload
+                               commons-io
+                               crypto-equality
+                               crypto-random
+                               org.clojure/tools.reader]]
                  [riemann-jvm-profiler "0.1.0"]])
