75a1e2445883291269112ea67a9f6ef688ac6512
Jan Stępień <jstepien@users.sourceforge.net>
2015-05-25 21:42:34 +0200

    Add a simple test

---
+++ test/flames/core_test.clj
@@ -0,0 +1,14 @@
+(ns flames.core-test
+  (:require [flames.core :refer [start! stop!]]
+            [clojure.test :refer [deftest is]]))
+
+(deftest test-basics
+  (let [spin-until #(if (< (System/nanoTime) %)
+                      (recur %))
+        p (start! {:port 54312, :host "localhost", :load 1, :dt 0.05})]
+    (try
+      (spin-until (+ 4e8 (System/nanoTime)))
+      (is (re-find #"flames\.core_test[^ ]+spin_until[^ ]+ invoke"
+                   (slurp "http://0:54312/flames.svg")))
+      (finally
+        (stop! p)))))
