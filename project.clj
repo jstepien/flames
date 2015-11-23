52e18f4555e12a92f222d9ed88d7fe251f808972
Jan Stępień <jstepien@users.sourceforge.net>
2015-11-23 18:29:11 +0100

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flames "0.2.0"
+(defproject flames "0.2.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"
                   :exclusions [clj-time
