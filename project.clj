07dd87b2c6420988978e2100e20f911b0a01af29
Jan Stępień <jan.stepien@innoq.com>
2018-05-25 09:13:22 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject flames "0.3.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.2"
-                  :exclusions [clj-time
-                               commons-fileupload
+                 [ring/ring-core "1.6.3"
+                  :exclusions [commons-fileupload
                                commons-io
                                crypto-equality
                                crypto-random
                                org.clojure/tools.reader]]
-                 [riemann-jvm-profiler "0.1.0"]])
+                 [riemann-jvm-profiler "0.1.0"
+                  :exclusions [clj-time]]
+                 [http-kit "2.3.0"]])
