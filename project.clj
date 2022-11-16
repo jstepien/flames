39750e1188c1fd5627e07a94fb1162dbd2db48f3
Jan Stępień <jan.stepien@innoq.com>
2022-11-16 17:40:06 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject flames "0.4.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.6.3"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.6"
                   :exclusions [commons-fileupload
                                commons-io
                                crypto-equality
                                crypto-random
                                org.clojure/tools.reader]]
-                 [riemann-jvm-profiler "0.1.0"
-                  :exclusions [clj-time]]
-                 [http-kit "2.3.0"]])
+                 [riemann-jvm-profiler "0.1.1"]
+                 [http-kit "2.6.0"]])
