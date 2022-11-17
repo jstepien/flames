9b9f611bd085413829df908acde4154e94c73e7e
Jan Stępień <jan.stepien@innoq.com>
2022-11-17 11:30:34 +0100

    Lower the Clojure requirement to 1.8.0
    It works with 1.8.0 and there's no need to enforce a newer version.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject flames "0.4.0"
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.9.6"
                   :exclusions [commons-fileupload
                                commons-io
                                crypto-equality
                                crypto-random
                                org.clojure/tools.reader]]
-                 [riemann-jvm-profiler "0.1.1"]
+                 [riemann-jvm-profiler "0.1.1"
+                  :exclusions [org.clojure/clojure]]
                  [http-kit "2.6.0"]])
